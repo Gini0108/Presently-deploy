f0d5eb28b8f9ba631bbd7ae1f83226d18395e34c
Gini0108 <ginik0108@gmail.com>
2022-07-24 15:35:41 +0200

    ✨ Added status page

--- index.ts
+++ index.ts
@@ -1,29 +1,13 @@
-import { oakCors } from "https://deno.land/x/cors@v1.2.2/mod.ts";
-import { Application } from "https://deno.land/x/oak@v10.6.0/mod.ts";
-import {
-  errorHandler,
-  limitHandler,
-  postHandler,
-} from "https://raw.githubusercontent.com/Schotsl/Uberdeno/main/middleware.ts";
+import Server from "https://raw.githubusercontent.com/Schotsl/Uberdeno/main/other/Server.ts";
 
 import systemRouter from "./router/systemRouter.ts";
 import socketRouter from "./router/socketRouter.ts";
 import fileRouter from "./router/fileRouter.ts";
 
-const application = new Application();
+const server = new Server();
 
-application.use(oakCors());
+server.add(fileRouter);
+server.add(systemRouter);
+server.add(socketRouter);
 
-application.use(errorHandler);
-application.use(limitHandler);
-application.use(postHandler);
-
-application.use(fileRouter.routes());
-application.use(systemRouter.routes());
-application.use(socketRouter.routes());
-
-application.use(fileRouter.allowedMethods());
-application.use(systemRouter.allowedMethods());
-application.use(socketRouter.allowedMethods());
-
-application.listen({ port: 8080 });
+server.listen();
