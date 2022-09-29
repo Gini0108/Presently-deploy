2fd0601e1828ed4270308b8cb5e670f8c4d42f29
GitHub <noreply@github.com>
2022-09-29 00:32:41 +0000

    ⬆️ Update dependencies

--- manager/IntervalManager.ts
+++ manager/IntervalManager.ts
@@ -1,5 +1,5 @@
 import { RequestInterval, RespondInterval, Worker } from "../types.ts";
-import { gray } from "https://deno.land/std@0.157.0/fmt/colors.ts";
+import { gray } from "https://deno.land/std@0.158.0/fmt/colors.ts";
 
 import AbstractManager from "./AbstractManager.ts";
 import WorkerRepository from "../repository/WorkerRepository.ts";
