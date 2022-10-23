e8c729938ffdb282e81e9843577064fcb348aea8
Gini0108 <ginik0108@gmail.com>
2022-10-23 23:07:40 +0200

    🐛 Fixed router bug

--- router/fileRouter.ts
+++ router/fileRouter.ts
@@ -7,11 +7,9 @@ const fileRouter = new GeneralRouter(
   "file",
 );
 
-const getStatus = fileController.getObjectStatus.bind(fileController);
 const addStatus = fileController.addObjectStatus.bind(fileController);
 
 // Add the custom route for the file status
-fileRouter.router.get("/entity/:uuid/status", getStatus);
 fileRouter.router.post("/entity/:uuid/status", addStatus);
 
 export default fileRouter.router;
