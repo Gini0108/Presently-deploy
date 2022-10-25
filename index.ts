aed31c8b8b77f77b53bc03f88b7f456034ba204f
Gini0108 <ginik0108@gmail.com>
2022-10-25 15:01:52 +0200

    💩 Added routers

--- index.ts
+++ index.ts
@@ -1,9 +1,13 @@
 import Server from "https://raw.githubusercontent.com/Schotsl/Uberdeno/main/other/Server.ts";
 
 import fileRouter from "./router/fileRouter.ts";
+import socketRouter from "./router/socketRouter.ts";
+import systemRouter from "./router/systemRouter.ts";
 
 const server = new Server();
 
 server.add(fileRouter);
+server.add(socketRouter);
+server.add(systemRouter);
 
 server.listen();
