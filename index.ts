7f4489c9d59370653cb0753931739a0c32326d9d
Gini0108 <ginik0108@gmail.com>
2022-11-10 22:27:38 +0100

    ✨ Added worker endpoint

--- index.ts
+++ index.ts
@@ -2,12 +2,14 @@ import Server from "https://raw.githubusercontent.com/Schotsl/Uberdeno/v1.0.0/ot
 
 import fileRouter from "./router/fileRouter.ts";
 import socketRouter from "./router/socketRouter.ts";
+import workerRouter from "./router/workerRouter.ts";
 import networkRouter from "./router/networkRouter.ts";
 
 const server = new Server();
 
 server.add(fileRouter);
 server.add(socketRouter);
+server.add(workerRouter);
 server.add(networkRouter);
 
 server.listen();
---
+++ router/workerRouter.ts
@@ -0,0 +1,18 @@
+import GeneralRouter from "https://raw.githubusercontent.com/Schotsl/Uberdeno/v1.0.0/router/GeneralRouter.ts";
+import GeneralController from "https://raw.githubusercontent.com/Schotsl/Uberdeno/v1.0.0/controller/GeneralController.ts";
+
+import WorkerEntity from "../entity/WorkerEntity.ts";
+import WorkerCollection from "../collection/WorkerCollection.ts";
+
+const workerController = new GeneralController(
+  "worker",
+  WorkerEntity,
+  WorkerCollection
+);
+
+const workerRouter = new GeneralRouter(
+  workerController,
+  "worker",
+);
+
+export default workerRouter.router;
