35352f4256d45b8bd33a99f7d1179e3c04037066
Gini0108 <ginik0108@gmail.com>
2022-11-27 19:42:16 +0100

    🐛 Fixed

--- entity/UserEntity.ts
+++ entity/UserEntity.ts
@@ -1,6 +1,9 @@
 import BaseEntity from "https://raw.githubusercontent.com/Schotsl/Uberdeno/v1.1.0/entity/BaseEntity.ts";
 
-import { UUIDColumn, VarcharColumn } from "https://raw.githubusercontent.com/Schotsl/Uberdeno/v1.1.0/other/Columns.ts";
+import {
+  UUIDColumn,
+  VarcharColumn,
+} from "https://raw.githubusercontent.com/Schotsl/Uberdeno/v1.1.0/other/Columns.ts";
 
 export default class FileEntity extends BaseEntity {
   public network = new UUIDColumn("network");
